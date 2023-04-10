d0adf5e1cc0ca5f1fb9b4557ae47aa6e2f7019e1
Michael <mickeymurage@gmail.com>
2023-04-10 06:33:02 +0300

    clean web static and setup web static with puppet

---
+++ 100-clean_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import os
+from fabric.api import *
+
+env.hosts = ['35.174.185.196', '54.157.141.97']
+env.user = 'ubuntu'
+env.identinty = '~/.ssh/school'
+
+
+def do_clean(number=0):
+    """
+    Delete out-of-date archives.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    all_archives = sorted(os.listdir("versions"))
+    [all_archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
