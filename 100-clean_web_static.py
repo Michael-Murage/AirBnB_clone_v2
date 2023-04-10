4c9d581ddc01ba08f70af35155b98867f3ba5eb6
Michael <mickeymurage@gmail.com>
2023-04-10 07:53:47 +0300

    clean web static correction

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,26 +1,57 @@
 #!/usr/bin/python3
 
-import os
+import os, sys, getopt
 from fabric.api import *
 
 env.hosts = ['35.174.185.196', '54.157.141.97']
-env.user = 'ubuntu'
-env.identinty = '~/.ssh/school'
+env.identity = '~/.ssh/school'
 
 
 def do_clean(number=0):
     """
     Delete out-of-date archives.
     """
-    number = 1 if int(number) == 0 else int(number)
+    try:
+        arg, val = getopt.getopt(sys.argv[1:], "i:u:")
+        ssh_key = None
+        user = None
+        for currArg, currVal in arg:
+            if currArg in ("-i"):
+                ssh_key = currVal
+            elif currArg in ('-u'):
+                user = currVal
+        
+        local("ssh {} -i {} -u {}".format(env.hosts[0] ,ssh_key, user))
 
-    all_archives = sorted(os.listdir("versions"))
-    [all_archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        number = 1 if int(number) == 0 else int(number)
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        all_archives = sorted(os.listdir("versions"))
+        [all_archives.pop() for i in range(number)]
+        with lcd("versions"):
+            [local("rm ./{}".format(a)) for a in archives]
+
+        with cd("/data/web_static/releases"):
+            archives = run("ls -tr").split()
+            archives = [a for a in archives if "web_static_" in a]
+            [archives.pop() for i in range(number)]
+            [run("rm -rf ./{}".format(a)) for a in archives]
+
+        local("exit")
+
+        local("ssh {} -i {} -u {}".format(env.hosts[1] ,ssh_key, user))
+
+        number = 1 if int(number) == 0 else int(number)
+
+        all_archives = sorted(os.listdir("versions"))
+        [all_archives.pop() for i in range(number)]
+        with lcd("versions"):
+            [local("rm ./{}".format(a)) for a in archives]
+
+        with cd("/data/web_static/releases"):
+            archives = run("ls -tr").split()
+            archives = [a for a in archives if "web_static_" in a]
+            [archives.pop() for i in range(number)]
+            [run("rm -rf ./{}".format(a)) for a in archives]
+    
+    except getopt.error as err:
+        print(str(err))
